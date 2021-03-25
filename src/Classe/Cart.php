3e22f77cddea39fd41276ba038228da31b22fb5c
parshimipopeli <philippe.lamorskipro@gmail.com>
2021-03-25 15:12:14 +0100

    add function increase article and increase article in cart

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -17,14 +17,13 @@ class Cart
 
     public function add($id)
     {
-$cart = $this->session->get('cart', []);
+        $cart = $this->session->get('cart', []);
 
-if (!empty($cart[$id])) {
-    $cart[$id]++;
-} else {
-    $cart[$id] = 1;
-}
-
+        if (!empty($cart[$id])) {
+            $cart[$id]++;
+        } else {
+            $cart[$id] = 1;
+        }
 
 
         $this->session->set('cart', $cart);
@@ -38,5 +37,27 @@ if (!empty($cart[$id])) {
     public function remove()
     {
         return $this->session->remove('cart');
+    }
+
+    public function delete($id)
+    {
+        $cart = $this->session->get('cart', []);
+        unset($cart[$id]);
+        return $this->session->set('cart', $cart);
+    }
+
+    public function decrease($id)
+    {
+        $cart = $this->session->get('cart', []);
+
+        if ($cart[$id] > 1) {
+            $cart[$id]--;
+        } else {
+            unset($cart[$id]);
+        }
+
+        return $this->session->set('cart', $cart);
+
+
     }
 }
--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,19 +3,41 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 class CartController extends AbstractController
 {
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+
+    }
+
+
     /**
      * @Route("/mon-panier", name="cart")
      */
     public function index(Cart $cart): Response
     {
-        dd($cart->get());
-        return $this->render('cart/index.html.twig');
+
+        $cartComplete = [];
+
+        if ($cart->get()) {
+            foreach ($cart->get() as $id => $quantity) {
+                $cartComplete[] = [
+                    'product' => $this->entityManager->getRepository(Product::class)->findOneById($id),
+                    'quantity' => $quantity
+                ];
+            }
+        }
+
+
+        return $this->render('cart/index.html.twig', [
+            'cart' => $cartComplete
+        ]);
     }
 
 
@@ -39,5 +61,28 @@ class CartController extends AbstractController
         $cart->remove('cart');
 
         return $this->redirectToRoute('products');
+    }
+
+    /**
+     * @Route("/cart/delete/{id}", name="delete-to-cart")
+     */
+    public function delete(Cart $cart, $id): Response
+    {
+
+        $cart->delete($id);
+
+        return $this->redirectToRoute('cart');
+    }
+
+
+    /**
+     * @Route("/cart/decrease/{id}", name="decrease-to-cart")
+     */
+    public function decrease(Cart $cart, $id): Response
+    {
+
+        $cart->decrease($id);
+
+        return $this->redirectToRoute('cart');
     }
 }
